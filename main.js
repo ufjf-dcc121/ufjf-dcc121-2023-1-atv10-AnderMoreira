3894c62b2aa8dbd90bd820f035e384f47836025c
Anderson Moreira <anderson.moreira@estudante.ufjf.br>
2023-05-30 20:40:36 -0300

    Isola estado

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import store from "./store.js"
+import { adicionar, getItems} from "./store.js";
 
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
@@ -11,7 +11,7 @@ function envia(evento)
     evento.preventDefault();
     console.log('Formulário Enviado!');
     const n = form.valor.value;
-    store.estado.push[n];
+    adicionar(n);
     form.valor.value = "";
     form.valor.focus();
     atualiza();
@@ -21,13 +21,12 @@ function atualiza()
 {
     const ol = document.querySelector('ol');
     ol.innerHTML = "";
-    
-    for(let i = 0; i< store.estado.length; i++)
-    {
+    const itens = getItems();
+    for(let i = 0; i < itens.length; i++) {
         const l1 = documento.createElement('li');
-        l1.textContent = store.estado(i);
+        l1.textContent = itens(i);
         ol.appendChild(li);
     }
-
+    itens().push("Boom!");
     
 }
--- store.js
+++ store.js
@@ -1,2 +1,9 @@
 const store = (estado [0, 1, 2, 3]);
-export  default store;
+
+export function adicionar(valor) {
+    store.estado.push[n];
+}
+
+export function getItems() {
+    return [...store.estado];
+}
